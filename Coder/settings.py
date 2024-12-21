5a7153db479f6cb7be185ec0c13f3dfa49e1e1c9
Sel <selcuk.80@outlook.de>
2024-12-21 13:17:30 +0100

    setting/correct

--- Coder/settings.py
+++ Coder/settings.py
@@ -33,13 +33,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
     "http://192.168.2.101:5501",
     "http://127.0.0.1:5501",
     "http://localhost:5501",
+    "http://localhost:5500",
 ]
 
 ROOT_URLCONF = 'Coder.urls'
