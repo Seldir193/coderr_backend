cfa0c8da4df789cf36c225a7e9951a875da9269b
Seldir193 <s_kocyigit@coderr.europe-west4-b.c.coderr-backend-457911.internal>
2025-04-26 21:18:21 +0000

    Root redirect '/' -> /admin and prod domain settings

--- Coder/urls.py
+++ Coder/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.http import HttpResponseRedirect 
 
 urlpatterns = [
+    path("", lambda r: HttpResponseRedirect("/admin/")),
     path("admin/", admin.site.urls),
     path("api/", include("coder_app.urls")),
 ]
